02052f701db0e0879cbcd5eaf7d05d8c6a0d4738
sumiransingh-droid <65755901+sumiransingh-droid@users.noreply.github.com>
2022-07-06 11:00:22 -0700

    Add files via upload

---
+++ java/lec 3/condition1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class condition1 {
+    
+    public static void Main(System args[]) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x%2==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }   
+    }
+}
